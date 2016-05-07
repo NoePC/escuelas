723278c02f834a83b8e989910e3c1f6626dc0a30
Miguel <miguela@revartsgaming.com>
2016-05-07 14:06:47 -0500

    Commint Inicial

---
+++ EscuelaTransparente/EscuelaTransparente/EscuelaTransparente/forms/bavanzada.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="bavanzada.aspx.cs" Inherits="EscuelaTransparente.forms.bavanzada" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div id="bavanzada">
+        <label>Busca Avanzada</label><br /><br />
+        <label>Escuela: </label>
+        <asp:TextBox ID="txtNEscuela" runat="server" ></asp:TextBox> <br /><br />
+        <label>Estado: </label>
+        <asp:TextBox ID="txtEstado" runat="server" ></asp:TextBox><br /><br />
+        <label>Municipio: </label>
+        <asp:TextBox ID="txtMunicipio" runat="server" ></asp:TextBox> <br /><br />
+        <label>CP: </label>
+        <asp:TextBox ID="txtCp" runat="server" ></asp:TextBox> <br /><br />
+    </div>
+    </form>
+</body>
+</html>
---
+++ EscuelaTransparente/EscuelaTransparente/EscuelaTransparente/forms/bavanzada.aspx.designer.cs
@@ -0,0 +1,60 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace EscuelaTransparente.forms {
+    
+    
+    public partial class bavanzada {
+        
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Control txtNEscuela.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNEscuela;
+        
+        /// <summary>
+        /// Control txtEstado.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEstado;
+        
+        /// <summary>
+        /// Control txtMunicipio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtMunicipio;
+        
+        /// <summary>
+        /// Control txtCp.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCp;
+    }
+}
